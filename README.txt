2f675e55dd6ed54a3b0c3f1e48caa46bdb038254
Zi Xu <sonixboost@gmail.com>
2024-08-09 18:15:03 +0800

    More robust error handling & a revamped number_of_case calculation

--- README.txt
+++ README.txt
@@ -1,5 +1,47 @@
-This is just a small project to help automate writing a report from data of an excel workbook.
+My objective of this small project is to design an application to help automate and simplify a report writing process. Conventionally, a typical report takes hours to complete and is very mundane. This program will generate a report in mere seconds, reducing the need for manual counting and typing.
 
-This project helped me create a usable GUI using customtkinter, as well as improve on my data manipulation and processing skill using pandas.
+This program will take in an excel sheet filled with data, pre-process it, extract important information and data and finally generate a docx report in a predefined format. Once the program runs, a GUI will appear. The GUI is created using customtkinter, a more modern python GUI library than its predecessor, tkinter.
 
-The program is easily launched by installing the standalone .exe file. The source code is available for references only.
+GUI components in appv2.py:
+
+“Open File” button instantiates a directory (default: program’s current directory) for users to choose the excel file.
+
+“Save To” button instantiates a directory (default: program’s current directory) for users to choose location to save the report.
+
+Textbox displays a status of the chosen excel file as well as the directory to which the report will be saved to.
+
+“Input custom file name..." allows users to input custom file name (default: Production report)
+
+“Generate!” button generates the docx report at a directory of users’ choosing.
+
+
+Code components in mainv2.py:
+In chronological order,
+
+1)	“Generate!” button, when pressed, will call generate_report(). It initialises a timer to record down the time taken for the entire process. 
+
+2)	Using pandas, the excel file is accessed and only columns 1,2,7,27 and 29 (“Status”, “Submission Date”, “Amount”, “Consultant name”, “Branch”) are selected and formed into a dataframe named data. 
+
+3)	data is then iterated through its “Status” column and have “REJECTED” rows removed.
+
+4)	date() is called and returns a list of 2 dates, the earliest and latest date found in the excel sheet. It is saved as a variable named date_output.
+
+5)	A custom filename check is conducted. A default name will be assigned to the report if no custom filename was given by users. It is saved as a variable named filename.
+
+6)	filename will be further given a directory to be saved at.
+
+7)	The report starts to be initialised. A para_header variable adds the header paragraph.
+
+8)	top_producer() is called, creating a global variable total_amount as well as adding the top producer paragraph into the report.
+
+9)	top_amount is then added to para_header¬.
+
+10)	top_case() is called, creating a global variable total_number_of_cases as well as adding the top cases paragraph into the report.
+
+11)	branch() is called, adding the top branches paragraph into the report.
+
+12)	The report ends off by adding the summarised numbers as well as words of encouragement. Report will also be saved and generated.
+
+13)	Timer will stop and the time taken will be calculated. A completion status will also be created. These stats can be seen in the app itself.
+
+
--- mainv2.py
+++ mainv2.py
@@ -1,20 +1,23 @@
 import datetime
 import pandas as pd
+from pandas._libs.tslibs.parsing import DateParseError
 import os
 from docx import Document
 
 #################### SETTINGS ########################
-EXCEL_SHEET = 'No File Selected'
+EXCEL_SHEET = 'No file selected.'
 required_cols = [1,2,7,27,29]
 save_path = os.getcwd()
+logging = True
 
 document = Document()
 # Set font
 style = document.styles['Normal']
-style.font.name = 'Calibri'
+style.font.name = 'Calibri' # type: ignore
 ########################################################
 
 def top_producer(producers: pd.DataFrame):
+    print("Generating producers...")
     producers_dict = {}
     for i,j in producers.iterrows():
         if j["Consultant Name"] not in producers_dict:
@@ -37,11 +40,13 @@ def top_producer(producers: pd.DataFrame):
                                style='List Number')
     document.add_paragraph()
     total_amount = ('{:,}\n'.format(total_amount))
+    print("Producers generated!:", sorted_producers, sep="\n")
 
-def top_case(cases: pd.DataFrame) -> tuple:
+def top_case(cases: pd.DataFrame) -> None:
+    print("Generating cases...")
     case_dict = {}
-    for i,j in cases.iterrows():
-        number_of_case = 2 if j["Amount"] == 3299 or j["Amount"] == 2804 else 1
+    for _,j in cases.iterrows():
+        number_of_case = calculate_case(j["Amount"])
         if j["Consultant Name"] in case_dict:
             case_dict[j["Consultant Name"]][1] += number_of_case
         else:
@@ -53,56 +58,118 @@ def top_case(cases: pd.DataFrame) -> tuple:
     document.add_paragraph("Top Cases:")
     global total_number_of_cases
     total_number_of_cases = 0
-    for cases in sorted_cases:
-        total_number_of_cases += cases[1][1]
-        if cases[1][1] <= 1:
-            document.add_paragraph("{0}~ {1} - {2} CASE".format(cases[1][0], cases[0], cases[1][1]),\
+    for indiv_cases in sorted_cases:
+        total_number_of_cases += indiv_cases[1][1]
+        if indiv_cases[1][1] <= 1:
+            document.add_paragraph("{0}~ {1} - {2} CASE".format(indiv_cases[1][0], indiv_cases[0], indiv_cases[1][1]),\
                                    style='List Number 2')
         else:
-            document.add_paragraph("{0}~ {1} - {2} CASES".format(cases[1][0], cases[0], cases[1][1]),\
+            document.add_paragraph("{0}~ {1} - {2} CASES".format(indiv_cases[1][0], indiv_cases[0], indiv_cases[1][1]),\
                                    style='List Number 2')
     document.add_paragraph()
+    print("Cases generated!:", sorted_cases, sep="\n")
     
-def branch(branches: pd.DataFrame) -> str:
+def branch(branches: pd.DataFrame) -> None:
+    print("Generating branches...")
     branch_dict = {}
-    for i,j in branches.iterrows():
-        number_of_case = 2 if j["Amount"] == 3299 or j["Amount"] == 2804 else 1
+    for _,j in branches.iterrows():
+        number_of_case = calculate_case(j["Amount"])
         if j["Branch"] not in branch_dict:
             branch_dict[j["Branch"]] = number_of_case
         else:
             branch_dict[j["Branch"]] += number_of_case
     # print(branch_dict)
-    sorted_cases = sorted(branch_dict.items(), key=lambda x: x[1], reverse=True)
+    sorted_branches = sorted(branch_dict.items(), key=lambda x: x[1], reverse=True)
     # print(sorted_cases)
 
     document.add_paragraph("Top Branches:")
-    for branch in sorted_cases:
+    for branch in sorted_branches:
         if branch[1] <= 1:
             document.add_paragraph("{0}~ {1} CASE".format(branch[0], branch[1]), style="List Number 3")
         else:
             document.add_paragraph("{0}~ {1} CASES".format(branch[0], branch[1]), style="List Number 3")
     document.add_paragraph()
+    print("Branches generated!:", sorted_branches, sep="\n")
 
-def date(dates: pd.DataFrame) -> list:
-    start_date = dates[0].replace('-', ' ')
-    end_date = dates[len(dates)-1].replace('-', ' ')
+def calculate_case(amount):
+    if amount in price_dict:
+        return price_dict[amount]
+    else:
+        for multiplier in range(2,11):
+            if amount%multiplier == 0:
+                return multiplier
+        return 1
+
+price_dict: dict = {
+    3299: 2,
+    2804: 2,
+    599: 1, 
+    899: 1, 
+    1499: 1, 
+    1799: 1
+}
+
+def date(dates: pd.Series) -> list:
+    print("Generating dates...")
+    dates_formatted: pd.Series = pd.to_datetime(dates).dt.strftime("%d/%m/%Y")
+    # print("Dates formatted:", dates_formatted)
+    start_date = dates_formatted[0].split('/')
+    start_date[1] = date_dict[start_date[1]]
+    start_date = " ".join(start_date)
+    end_date = dates_formatted[len(dates_formatted)-2].split('/')
+    end_date[1] = date_dict[end_date[1]]
+    end_date = " ".join(end_date)
+    print("Dates generated!")
+    print("Start date:", start_date)
+    print("End date:", end_date)
     return [start_date, end_date]
+
+date_dict = {
+    "01": "Jan",
+    "02" : "Feb",
+    "03" : "Mar",
+    "04" : "Apr",
+    "05" : "May",
+    "06" : "Jun",
+    "07" : "Jul",
+    "08" : "Aug",
+    "09" : "Sep",
+    "10" : "Oct",
+    "11" : "Nov",
+    "12" : "Dec"
+}
 
 def generate_report(custom_name: str):
     try:
         ########### TO WRITE ##############
+        global report
         dt1 = datetime.datetime.now()
 
+        print("Parsing excel sheet...:", EXCEL_SHEET)
         data = pd.read_excel(EXCEL_SHEET, usecols= required_cols)
         for i,j in data.iterrows():
-            data = data.replace(j["Branch"], j["Branch"].split(" ").pop())
+            
+            if pd.isnull(j["Status"]):
+                raise ValueError("Empty cell in 'Status' column in excel sheet")
+            if pd.isnull(j["Submission Date"]):
+                raise ValueError("Empty cell in 'Submission Date' column in excel sheet")
+            if pd.isnull(j["Amount"]):
+                raise ValueError("Empty cell in 'Amount' column in excel sheet")
+            if pd.isnull(j["Consultant Name"]):
+                raise ValueError("Empty cell in 'Consultant Name' column in excel sheet")
+            if pd.isnull(j["Branch"]):
+                raise ValueError("Empty cell in 'Branch' column in excel sheet")
+            
             if j["Status"] == "REJECTED":
                 data = data.drop(i)
-        # print(data)
-        # print(data[["Branch","Consultant Name","Amount"]])
-        date_output = date(data["Submission Date"])
+            data = data.replace(j["Branch"], j["Branch"].split(" ").pop())
+        print("Parsing successful!", "Dataframe:", data, sep='\n')
 
-        global report
+        print("Generating submission dates...")
+        date_output = date(data["Submission Date"])
+        print("Submission dates generated!:", date_output)
+
+        print("Generating filename...")
         if not custom_name:
             filename = "Production Report {0}".format(datetime.datetime.now().isoformat(timespec='seconds'))
             filename = filename.replace(":", '-')
@@ -110,31 +177,45 @@ def generate_report(custom_name: str):
             filename = "{0}".format(custom_name)
 
         filename = os.path.join(save_path, filename + ".docx")
-        # processed = processed_data
-        # print(filename)
+        print("Filename generated!:", filename)
 
         print("Generating report...")
         para_header = document.add_paragraph("Month-to-date Production:\n{0} - {1}\nRM "\
                                              .format(date_output[0], date_output[1]))
         
         top_producer(data[["Branch","Consultant Name", "Amount"]])
-        para_header.add_run(total_amount)
+        para_header.add_run(str(total_amount))
         top_case(data[["Branch", "Consultant Name", "Amount"]])
         branch(data[["Branch", "Amount"]])
         document.add_paragraph("TOTAL CASES: "+ str(total_number_of_cases)+ " CASES\n")
 
         document.add_paragraph("Your dedication to protecting and preserving family wealth distribution is truly \
 commendable. You are not just selling products; you are safeguarding the future and \
-ensring the well-being of countless families. \n\nKeep up the fantastic work, and may \
+ensuring the well-being of countless families. \n\nKeep up the fantastic work, and may \
 your efforts continue to help secure the financial legacies of many more families.\
 \n\nOnce again, congratulations on your well-deserved success!")
         
         document.save(filename)
-    
-    except Exception as e:
-        report = "Something went wrong. Please try again later."
+
+    except FileNotFoundError as e:
+        report = ("Error: File not found. Please check the provided path.\nInfo: {0}").format(e)
         print(report)
-        print(e)
+        return
+
+    except DateParseError as e:
+        report = ("Error: Date format not recognized. Please use DD-MM-YY.\nInfo: {0}").format(e)
+        print(report)
+        return
+
+    except ValueError as e:
+        report = ("Error: File format not recognized or cells could be empty. Please check file format or empty cells.\nInfo: {0}").format(e)
+        print(report)
+        return
+    
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
     
     else:
         dt2 = datetime.datetime.now()
